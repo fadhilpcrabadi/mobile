a3c428816b25353d6c8fbe26c066012305aa3bdd
Fransiskus <fransiskus24ti@mahasiswa.pcr.ac.id>
2026-09-24 14:38:18 +0700

    First Commit

---
+++ app/src/main/java/com/example/fadhil3tie/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,43 @@
+package com.example.fadhil3tie.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fadhil3tie.R
+import com.example.fadhil3tie.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val inputNoTujuan: EditText = findViewById(R.id.inputNoTujuan)
+        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+
+            val noTujuan = binding.inputNoTujuan.text
+            Toast.makeText(this, "Pesan berhasil dikirim ke $noTujuan", Toast.LENGTH_SHORT).show()
+
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/fadhil3tie/pertemuan_4/FourthActivity.kt
@@ -0,0 +1,31 @@
+package com.example.fadhil3tie.pertemuan_4
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fadhil3tie.MainActivity
+import com.example.fadhil3tie.R
+import com.example.fadhil3tie.databinding.ActivityFourthBinding
+import com.example.fadhil3tie.databinding.ActivityMainBinding
+
+class FourthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityFourthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityFourthBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btnToFourth.setOnClickListener {
+            val i = Intent(this@FourthActivity, MainActivity::class.java)
+            startActivity(i)
+        }
+    }
+}
