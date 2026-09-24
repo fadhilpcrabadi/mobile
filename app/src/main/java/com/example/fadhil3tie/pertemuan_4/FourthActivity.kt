cb09c365c6a80f7f39c55abba619692f04f0fb20
Fransiskus <fransiskus24ti@mahasiswa.pcr.ac.id>
2026-09-24 15:06:45 +0700

    Second Commit

--- app/src/main/java/com/example/fadhil3tie/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/fadhil3tie/pertemuan_4/FourthActivity.kt
@@ -30,8 +30,9 @@ class FourthActivity : AppCompatActivity() {
         Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
 
         binding.btnToFourth.setOnClickListener {
-            val i = Intent(this@FourthActivity, MainActivity::class.java)
-            startActivity(i)
+//            val i = Intent(this@FourthActivity, MainActivity::class.java)
+//            startActivity(i)
+            finish()
         }
     }
 }
