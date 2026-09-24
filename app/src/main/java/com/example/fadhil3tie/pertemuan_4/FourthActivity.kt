9c7d1e8137084408f25b7a1c9cb24390ad040f47
Fransiskus <fransiskus24ti@mahasiswa.pcr.ac.id>
2026-09-24 14:54:53 +0700

    Second Commit

--- app/src/main/java/com/example/fadhil3tie/pertemuan_4/FourthActivity.kt
+++ app/src/main/java/com/example/fadhil3tie/pertemuan_4/FourthActivity.kt
@@ -2,6 +2,7 @@ package com.example.fadhil3tie.pertemuan_4
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +24,11 @@ class FourthActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val name = intent.getStringExtra("name")
+        val from = intent.getStringExtra("from")
+        val age = intent.getIntExtra("age",0)
+        Log.e("Data Intent","Nama: $name , Usia: $age, Asal: $from")
+
         binding.btnToFourth.setOnClickListener {
             val i = Intent(this@FourthActivity, MainActivity::class.java)
             startActivity(i)
